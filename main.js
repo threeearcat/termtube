112ff74d0aa88d596528eae761d925d69d09e1cf
Dae R. Jeong <threeearcat@gmail.com>
2021-12-23 11:05:58 +0900

    auth

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+'use strict'
+
+const auth = require(__dirname + "/auth.js");
+
+function auth_callback(auth) {
+		console.log("auth done");
+}
+
+auth.authorize(auth_callback);
